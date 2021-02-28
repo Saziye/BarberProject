eface283ee565f1bcb12b6cec37be6feb16d9619
Saziye <yilmazersaziye@gmail.com>
2021-02-28 19:03:13 +0300

    Added config files

---
+++ src/config/settings.js
@@ -0,0 +1,19 @@
+/**
+ * Basic Setting Variables Define
+ */
+export const BaseSetting = {
+    defaultLanguage: 'tr',
+    languageSupport: [
+        'tr',
+        'en',
+        'fr',
+    ],
+    resourcesLanguage: {
+        tr: {
+            translation: require('../lang/tr.json'),
+        },
+        en: {
+            translation: require('../lang/en.json'),
+        },
+    },
+};
